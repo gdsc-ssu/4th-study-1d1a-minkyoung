b804bd8f6d01d0bc68b6563bf115f6b57fe772ad
shinminkyoung1 <dottiee99@naver.com>
2024-11-10 21:53:02 +0900

    [11/10] 1025: 제곱수 찾기

--- 1013.cpp
+++ 1013.cpp
@@ -1,36 +1,28 @@
-#include<iostream>
+#include <iostream>
+#include <regex>
 
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+void Solve()
+{
+    int t;
+    string s;
+    cin >> t;
+    regex re("(([1][0][0]+[1]+)|([0][1]))+");
+    while (t--) 
+    {
+        cin >> s;
+        if (regex_match(s, re))
+            cout << "YES" << '\n';
+        else
+            cout << "NO" << '\n';
+    }
+}
 
-	int t;
-	cin >> t;
-
-	for (int i = 0; i < t; i++) {
-		long long s, e;
-		cin >> s >> e;
-
-		long long n = 1;
-		long long dis = e - s - 2;
-
-		if (dis <= 0) {
-			cout << dis + 2 << "\n";
-			continue;
-		}
-
-		while ((n*n + 3 * n) < dis) {
-			n++;
-		}
-
-		if ((n*n + 2 * n) <= dis) n *= 2;
-		else n = n * 2 - 1;
-
-		cout << n + 2 << "\n";
-	}
-
-	return 0;
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    Solve();
+    return 0;
 }
